0297237b751ea68b90c9367753885a51d8480d4a
Ray <raylu511@gmail.com>
2022-12-23 11:53:00 -0500

    Create app boilerplate

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginScreen from "../screens/LoginScreen";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginScreen />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
