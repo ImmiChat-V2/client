1c19183aaa94467aa01e5ba178ef0798b60098d0
Ray <raylu511@gmail.com>
2022-12-29 11:40:22 -0500

    Created shared UserProfileWithTimestamp component

---
+++ src/shared/components/UserProfileWithTimestamp.tsx
@@ -0,0 +1,35 @@
+import { Avatar, Box, Typography } from "@mui/material";
+import { formatTime } from "../utils/timeUtils";
+
+type UserProfileWithTimestampProps = {
+  readonly profilePicture?: string;
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly timestamp: Date;
+  readonly boxProps?: Record<string, string>;
+};
+
+const UserProfileWithTimestamp = ({
+  profilePicture,
+  firstName,
+  lastName,
+  timestamp,
+  boxProps,
+}: UserProfileWithTimestampProps) => {
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+      sx={{ ...boxProps }}
+    >
+      <Avatar src={profilePicture}></Avatar>
+      <Typography variant="subtitle1" paddingRight="11px" fontWeight="bold">
+        {firstName} {lastName}
+      </Typography>
+      <Typography variant="caption" color="#65676B">{formatTime(timestamp)}</Typography>
+    </Box>
+  );
+};
+
+export default UserProfileWithTimestamp;
--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,1 +1,2 @@
 export { default as Dropdown } from "./Dropdown";
+export { default as UserProfileWithTimestamp } from "./UserProfileWithTimestamp";
