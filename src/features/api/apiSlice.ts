720eaa1df44a21015e4ed89c85fd49f3eed255c0
Matthew Tan <tanmatt1515@gmail.com>
2022-12-30 12:29:03 -0500

    changed port number

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -1,7 +1,7 @@
 // Import the RTK Query methods from the React-specific entry point
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "http://localhost:5000";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
