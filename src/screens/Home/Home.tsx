22fa81ebc77afe1603137b2467a7768ed7e04915
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2023-01-03 19:01:12 -0800

    updated stylings from css to sx props

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import Post from "../../shared/components/post/Post";
 
 const Home = () => {
   return (
     <div>
       <Link to={"/login"}>dasd</Link>
+      <Post />
     </div>
   );
 };
