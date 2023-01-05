c3ae330bd710be8394713b103e40fda880224fe7
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2023-01-05 10:45:23 -0800

    split post component into 3 smaller components

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -11,5 +11,21 @@ const Home = () => {
     </Box>
   );
 };
+/*
+id
+userId
+top: 
+  profilePic
+  firstName 
+  lastName
+  timePosted
 
+mid: 
+  content(text)
+  post image?
+
+Bottom: 
+  like count
+  comment count
+*/
 export default Home;
