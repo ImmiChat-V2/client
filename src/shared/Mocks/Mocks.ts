d38d658fa7d2df7e3c5713d660e9998c835e46df
Emir Akhmetov <91210955+emir4ik-code@users.noreply.github.com>
2023-01-06 16:19:00 -0500

    minor changes to mock data

--- src/shared/Mocks/Mocks.ts
+++ src/shared/Mocks/Mocks.ts
@@ -1,4 +1,4 @@
-const userOneProfilePic =
+const profilePic =
   "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/255700933_3056178267990272_2842286584796059820_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=A80f0zuNGfAAX9PJM9Q&_nc_ht=scontent-lga3-1.xx&oh=00_AfDKwIGJBkGU3eDCRmQDF8-_TV57x4I8XmqSOgR_EadBUg&oe=63BD1968";
 
 export const messageArray = [
@@ -6,43 +6,41 @@ export const messageArray = [
     name: "Ray Lu",
     message: "U tryna get there at 10? U tryna get there at 10?",
     isCurrentUser: true,
-    profilePic: userOneProfilePic,
+    profilePic: profilePic,
   },
   {
     name: "Ray Lu",
     message: "U tryna get there at 10? U tryna get there at 10?",
     isCurrentUser: false,
-    profilePic: userOneProfilePic,
+    profilePic: profilePic,
   },
   {
     name: "Ray Lu",
     message: "U tryna get there at 10? U tryna get there at 10?",
     isCurrentUser: true,
-    profilePic: userOneProfilePic,
+    profilePic: profilePic,
   },
   {
     name: "Ray Lu",
     message: "U tryna get there at 10? U tryna get there at 10?",
-    profilePic: userOneProfilePic,
+    profilePic: profilePic,
     isCurrentUser: false,
   },
   {
     name: "Ray Lu",
     message: "U tryna get there at 10? U tryna get there at 10?",
     isCurrentUser: true,
-    profilePic: userOneProfilePic,
+    profilePic: profilePic,
   },
   {
     name: "Ray Lu",
     message: "U tryna get there at 10? U tryna get there at 10?",
     isCurrentUser: true,
-    profilePic: userOneProfilePic,
+    profilePic: profilePic,
   },
 ];
 
-const profilePic =
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfrQ0_XW-hZ9z4pwXUt8RSt_9NkxQjTSO-QjTEPLBafQ&s";
-const connections = [
+export const connectionsArray = [
   {
     firstName: "Ray",
     lastName: "Lu",
