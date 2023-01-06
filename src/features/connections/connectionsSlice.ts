b4bf812791cf6937e0ef79f862d259e470fa5b99
Emir Akhmetov <91210955+emir4ik-code@users.noreply.github.com>
2023-01-06 18:28:42 -0500

    some query work

--- src/features/connections/connectionsSlice.ts
+++ src/features/connections/connectionsSlice.ts
@@ -1,2 +1,18 @@
-// boiler plate
-import React from "react";
+import { createSlice } from "@reduxjs/toolkit";
+import { BaseConnectionModel } from "./models/Connections.model";
+
+type initialStateType = {
+    connections: BaseConnectionModel[] | null;
+}
+
+const initialState:initialStateType = {
+    connections : null
+}
+
+const connectionSlice = createSlice({
+    name: "connections",
+    initialState: initialState,
+    reducers: {
+        
+    }
+})
--- src/features/connections/services/connectionsApiSlice.ts
+++ src/features/connections/services/connectionsApiSlice.ts
@@ -1,2 +1,32 @@
 // boiler plate
-import React from "react";
+import { BaseUserModel } from "features/auth/models/User.model";
+import { apiSlice } from "../../api/apiSlice";
+
+export const connectionsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getConnections: builder.mutation({
+      query: (userId) => ({
+        url: `/users/${userId}/connections`,
+        method: "GET",
+      }),
+    }),
+    sendConnectionRequest: builder.mutation({
+      query: (userId) => ({
+        url: `/users/${userId}/connections`,
+        method: "POST",
+      }),
+    }),
+    acceptConnection: builder.mutation({
+      query: (userId) => ({
+        url: `/users/${userId}/connections`,
+        method: "PUT",
+      }),
+    }),
+    removeConnection: builder.mutation({
+      query: (userId) => ({
+        url: `/users/${userId}/connections`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
