84f25d4a7cc26869e82b5d5a031a7aab68314009
Ray <raylu511@gmail.com>
2023-01-06 13:42:30 -0500

    Misc. stuff

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Dropdown } from "./Dropdown";
-export { default as UserProfileWithTimestamp } from "./UserProfileWithTimestamp";
+export { default as UserProfileWidget } from "./UserProfileWidget";
 export { default as SharePost } from "./SharePost";
 export { default as FadeDropdown } from "./FadeDropdown";
