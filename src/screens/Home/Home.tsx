016efb5dac6918be55d9f157cc990186751aaac8
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2023-01-06 18:08:14 -0800

    updated useTheme hook

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./home.css";
 
 const Home = () => {
-  const { theme } = useTheme()
+  // const { theme } = useTheme()
   return (
     <Box>
       <Link to={"/login"}>dasd</Link>
