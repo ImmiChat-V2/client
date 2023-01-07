a9467cdf7e771551234ddf63e9aef09055033eed
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2023-01-06 18:19:46 -0800

    fixed minor issues

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -3,28 +3,11 @@ import { Link } from "react-router-dom";
 import "./home.css";
 
 const Home = () => {
-  // const { theme } = useTheme()
   return (
     <Box>
       <Link to={"/login"}>dasd</Link>
     </Box>
   );
 };
-/*
-id
-userId
-top: 
-  profilePic
-  firstName 
-  lastName
-  timePosted
 
-mid: 
-  content(text)
-  post image?
-
-Bottom: 
-  like count
-  comment count
-*/
 export default Home;
