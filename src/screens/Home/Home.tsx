129d6ba6d622451b81d2ed08ac8fd17c32664b41
Daywick <derekli11204@gmail.com>
2023-01-08 06:00:42 -0500

    stash

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,10 +1,12 @@
 import { Box } from "@mui/material";
 import { Link } from "react-router-dom";
 import "./home.css";
+import { Navbar } from "shared/components/navbar";
 
 const Home = () => {
   return (
     <Box>
+      <Navbar />
       <Link to={"/login"}>dasd</Link>
       <Link to={"/profile"}>Profile</Link>
     </Box>
