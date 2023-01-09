7262f5e776736595b06fee81898bf01b5df8a63b
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2023-01-08 18:14:00 -0800

    changed post top to use profile widget

---
+++ src/shared/components/post/Post.tsx
@@ -0,0 +1,66 @@
+import { Box } from "@mui/material";
+import PostTop from "./PostTop";
+import PostBody from "./PostBody";
+import PostFooter from "./PostFooter";
+import useTheme from "../../../features/theme/useTheme";
+import "./post.css";
+
+type BasePost = {
+  readonly id: number;
+  readonly userId: number;
+  readonly profilePic?: string;
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly timestamp?: Date,
+  readonly content: string;
+  readonly media?: string;
+};
+
+function Post({
+  id,
+  userId,
+  profilePic,
+  firstName,
+  lastName,
+  timestamp,
+  content,
+  media,
+}: BasePost) {
+  const {
+    themeColor: { backgroundColor },
+  } = useTheme();
+  return (
+    <Box
+      component="div"
+      sx={{
+        maxHeight: "630px",
+        maxWidth: "500px",
+        backgroundColor,
+        borderRadius: "10px",
+        mb: "100px",
+      }}
+    >
+      <Box
+        component="div"
+        sx={{
+          mr: "20px",
+          ml: "20px",
+        }}
+      >
+        <PostTop
+          id={id}
+          userId={userId}
+          profilePic={profilePic}
+          firstName={firstName}
+          lastName={lastName}
+          timeStamp={timestamp}
+        />
+        <PostBody id={id} userId={userId} content={content} media={media} />
+        <hr style={{ marginTop: "20px" }}></hr>
+        <PostFooter id={id} userId={userId} />
+      </Box>
+    </Box>
+  );
+}
+
+export default Post;
