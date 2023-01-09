fdd0a69bcd8ba3ef711773040cc4c957c98e22d2
Matthew Tan <tanmatt1515@gmail.com>
2023-01-08 22:55:34 -0500

    camel cased function name

--- src/shared/utils/UploadMedia.ts
+++ src/shared/utils/UploadMedia.ts
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-const UploadMedia = async (files: FileList) => {
+const uploadMedia = async (files: FileList) => {
   const formData = new FormData();
   formData.append("file", files[0]);
   formData.append("upload_preset", "ds2dkkvk");
@@ -20,4 +20,4 @@ const UploadMedia = async (files: FileList) => {
   }
 };
 
-export default UploadMedia;
+export default uploadMedia;
