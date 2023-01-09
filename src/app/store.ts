1a0971e0e15e3279a495ac6ad1af350ea637fc50
Daywick <derekli11204@gmail.com>
2023-01-09 16:45:10 -0500

    stash

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,12 +3,15 @@ import { apiSlice } from "../features/api/apiSlice";
 import authReducer from "../features/auth/authSlice";
 import darkModeSlice from "../features/theme/darkModeSlice";
 import connectionsSlice from "features/connections/connectionsSlice";
+import userProfileSlice from "features/userprofile/userProfileSlice";
+
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
     darkMode: darkModeSlice,
     connections: connectionsSlice,
+    userprofile: userProfileSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
