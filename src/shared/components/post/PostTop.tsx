7480b41554ec24cbbd09185cb1dfa48fb00671d8
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2023-01-08 18:11:25 -0800

    changed post top to use profile widget

--- src/shared/components/post/PostTop.tsx
+++ src/shared/components/post/PostTop.tsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import { Box, IconButton, Typography } from "@mui/material";
 import { PersonAdd, PersonRemoveAlt1 } from "@mui/icons-material/";
+import { UserProfileWidget } from "shared/components";
 import useTheme from "../../../features/theme/useTheme";
 
 type PostTop = {
   readonly id: number;
   readonly userId: number;
-  readonly profilePic: string;
+  readonly profilePic?: string;
   readonly firstName: string;
   readonly lastName: string;
-  readonly timePosted?: string;
+  readonly timeStamp?: Date;
 };
 
 function PostTop({
@@ -18,12 +19,8 @@ function PostTop({
   profilePic,
   firstName,
   lastName,
-  // timePosted,
-}: 
-PostTop) {
-  const {
-    themeColor: { color },
-  } = useTheme();
+  timeStamp,
+}: PostTop) {
   const [isFriend, setIsFriend] = useState(false);
   const handleFriendClick = () => {
     setIsFriend(!isFriend);
@@ -39,18 +36,6 @@ PostTop) {
         }}
         className="post-top"
       >
-        <Box component="div" sx={{ mr: "10px" }}>
-          <Box
-            component="img"
-            sx={{
-              width: "50px",
-              height: "50px",
-              borderRadius: "50px",
-              cursor: "pointer",
-            }}
-            src={profilePic}
-          ></Box>
-        </Box>
         <Box
           component="div"
           className="post-user-info-and-utilities"
@@ -61,14 +46,11 @@ PostTop) {
             width: "100%",
           }}
         >
-          <Box component="span" sx={{ cursor: "pointer" }}>
-            <Typography sx={{ color }}>
-              {firstName} {lastName}
-            </Typography>
-            <Typography color="#858585" sx={{ fontSize: "12px" }}>
-              {/* {timePosted} */}
-            </Typography>
-          </Box>
+          <UserProfileWidget
+            firstName="Fake"
+            lastName="User"
+            boxProps={{ cursor: "pointer" }}
+          />
           <Box component="span" sx={{ display: "flex", cursor: "pointer" }}>
             <IconButton
               onClick={handleFriendClick}
