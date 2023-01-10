6ac0f26919d849230db57a600ada66116bfeeca7
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2023-01-09 16:40:20 -0800

    cleaned up imports and added env for url

--- src/shared/components/post/PostBody.tsx
+++ src/shared/components/post/PostBody.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from "@mui/material";
-import useTheme from "../../../features/theme/useTheme";
+import useTheme from "features/theme/useTheme";
 
 type PostBody = {
   readonly id: number;
