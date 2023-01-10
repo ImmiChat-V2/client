19b7dc6576754ad6794efafa1bf5420fa0452583
Emir Akhmetov <91210955+emir4ik-code@users.noreply.github.com>
2023-01-09 22:08:14 -0500

    added back include credentials

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -1,10 +1,11 @@
 // Import the RTK Query methods from the React-specific entry point
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = "http://localhost:3000";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
+    credentials: "include",
     mode: "cors",
   }),
   endpoints: (builder) => ({}),
