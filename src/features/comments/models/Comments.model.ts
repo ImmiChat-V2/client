1c75b8a79da38f61c8b3bd4899d44be3eecc7562
Daywick <derekli11204@gmail.com>
2023-01-12 17:57:42 -0500

    comment api slice

--- src/features/comments/models/Comments.model.ts
+++ src/features/comments/models/Comments.model.ts
@@ -40,32 +40,3 @@ export type BaseCommentPropType = {
   isUser: boolean;
   data: BaseCommentModel;
 };
-export type BaseCommentModel = {
-  readonly id: number;
-  readonly userId: number;
-  readonly postId: number;
-  readonly media?: string;
-  readonly content: string;
-  readonly createdAt: Date;
-  readonly updatedAt: Date;
-};
-
-export type UpdateCommentRequestModel = Pick<
-  BaseCommentModel,
-  "media" | "content"
->;
-export type DeleteCommentRequestModel = Pick<BaseCommentModel, "userId">;
-export type CreateCommentRequestModel = Omit<
-  BaseCommentModel,
-  "id" | "createdAt" | "updatedAt"
->;
-
-export type UpdateCommentProps = {
-  comment_id: string;
-  body: UpdateCommentRequestModel;
-};
-
-export type DeleteCommentProps = {
-  comment_id: string;
-  body: DeleteCommentRequestModel;
-};
