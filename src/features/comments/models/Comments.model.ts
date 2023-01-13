1417e25f19c09268dda4000c6e4da78edee7fd76
Daywick <derekli11204@gmail.com>
2023-01-13 06:00:12 -0500

    create like and delete like

--- src/features/comments/models/Comments.model.ts
+++ src/features/comments/models/Comments.model.ts
@@ -1,3 +1,5 @@
+import { BaseUserModel } from "features/auth/models/User.model";
+
 export type BaseCommentModel = {
   readonly id: number;
   readonly userId: number;
@@ -16,6 +18,11 @@ export type DeleteCommentRequestModel = Pick<BaseCommentModel, "userId">;
 export type CreateCommentRequestModel = Omit<
   BaseCommentModel,
   "id" | "createdAt" | "updatedAt"
+>;
+
+export type UsersLikedCommentModel = Pick<
+  BaseUserModel,
+  "firstName" | "lastName" | "profilePic"
 >;
 
 export type UpdateCommentProps = {
