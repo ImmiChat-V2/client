a0aa0deb1ed5d74ba73204bc4c6a6b9b80503191
Daywick <derekli11204@gmail.com>
2023-01-13 06:00:12 -0500

    create like and delete like

--- src/features/comments/services/commentApiSlice.ts
+++ src/features/comments/services/commentApiSlice.ts
@@ -4,6 +4,7 @@ import {
   UpdateCommentProps,
   DeleteCommentProps,
   CreateCommentRequestModel,
+  UsersLikedCommentModel,
 } from "../models/Comments.model";
 
 export const commentApiSlice = apiSlice.injectEndpoints({
@@ -33,9 +34,27 @@ export const commentApiSlice = apiSlice.injectEndpoints({
       }),
     }),
     getCommentsForPost: builder.query<BaseCommentModel[], string>({
-      query: (post_id) => ({
-        url: `/posts/${post_id}/comments`,
+      query: (postId) => ({
+        url: `/posts/${postId}/comments`,
         method: "GET",
+      }),
+    }),
+    getLikesFromComment: builder.query<UsersLikedCommentModel, string>({
+      query: (commentId) => ({
+        url: `/comments/${commentId}/likes`,
+        method: "GET",
+      }),
+    }),
+    likeComment: builder.query<any, string>({
+      query: (commentId) => ({
+        url: `/comments/${commentId}/likes`,
+        method: "POST",
+      }),
+    }),
+    deleteCommentLike: builder.query<any, string>({
+      query: (commentId) => ({
+        url: `/comments/${commentId}/likes`,
+        method: "DELETE",
       }),
     }),
   }),
