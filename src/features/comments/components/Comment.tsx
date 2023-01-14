5da421e6be2eb38f305983e8cf6dc9c90a0a4ec0
Daywick <derekli11204@gmail.com>
2023-01-14 16:11:38 -0500

    fix

---
+++ src/features/comments/components/Comment.tsx
@@ -0,0 +1,11 @@
+import { Card, Typography } from "@mui/material";
+
+const BaseComment = ({ isPost, data }: any) => {
+  return (
+    <Card>
+      <Typography>The Theory the the thumb was a thigh</Typography>
+    </Card>
+  );
+};
+
+export default BaseComment;
