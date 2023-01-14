0fc03f1e38d083d6498cd33d487d9e56e64bdd07
Daywick <derekli11204@gmail.com>
2023-01-14 18:38:30 -0500

    stash

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 import { Link } from "react-router-dom";
 import { Navbar } from "shared/components/navbar";
+import { Comment } from "features/comments/components";
 import Post from "shared/components/post/Post";
 
 const Home = () => {
@@ -9,15 +10,16 @@ const Home = () => {
       <Navbar />
       <Link to={"/login"}>dasd</Link>
       <Link to={"/profile"}>Profile</Link>
-      <Post 
-      id={1}
-      userId={1}
-      profilePic={""}
-      firstName={"Fake"}
-      lastName={"User"}
-      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor accusantium consequuntur fuga expedita reiciendis nemo, nesciunt assumenda cumque vel architecto itaque quibusdam deserunt, harum magni quam ab, quas alias ea."
-      media="https://media.istockphoto.com/id/615398376/photo/new-york-city-nyc-usa.jpg?s=612x612&w=0&k=20&c=rlrsrt4jbORPDSOW5df06Ik_X_5iQo1rYQd53xSs4nw="
+      <Post
+        id={1}
+        userId={1}
+        profilePic={""}
+        firstName={"Fake"}
+        lastName={"User"}
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor accusantium consequuntur fuga expedita reiciendis nemo, nesciunt assumenda cumque vel architecto itaque quibusdam deserunt, harum magni quam ab, quas alias ea."
+        media="https://media.istockphoto.com/id/615398376/photo/new-york-city-nyc-usa.jpg?s=612x612&w=0&k=20&c=rlrsrt4jbORPDSOW5df06Ik_X_5iQo1rYQd53xSs4nw="
       />
+      <Comment />
     </Box>
   );
 };
