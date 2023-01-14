88296751aa986f900cd875c834b03eff1b47ab0b
Daywick <derekli11204@gmail.com>
2023-01-14 18:38:30 -0500

    stash

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,14 +1,14 @@
 import { Navbar } from "shared/components/navbar";
 import { Footer } from "shared/components/footer";
 import HomePageBody from "./HomePageBody";
+import { Comment } from "features/comments/components";
+import Post from "shared/components/post/Post";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <HomePageBody />
-      <Footer />
-    </>
+    </Box>
   );
 };
 
