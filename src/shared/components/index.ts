296b7ad469209a3619d7c8ddba805b3e647c500e
Ray <raylu511@gmail.com>
2023-01-16 18:41:56 -0500

    Updated imports

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Dropdown } from "./Dropdown";
 export { default as UserProfileWidget } from "./UserProfileWidget";
-// export { default as SharePost } from "./SharePost";
+export { default as SharePost } from "./SharePost";
 export { default as FadeDropdown } from "./FadeDropdown";
