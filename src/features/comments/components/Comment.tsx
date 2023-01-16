f6657ae1e019cc1ea9531126b58996f66c9b6b45
Daywick <derekli11204@gmail.com>
2023-01-16 13:33:06 -0500

    fix

--- src/features/comments/components/Comment.tsx
+++ src/features/comments/components/Comment.tsx
@@ -11,35 +11,8 @@ import { AddComment, Favorite } from "@mui/icons-material";
 import { BaseCommentPropType } from "../models/Comments.model";
 import useTheme from "features/theme/useTheme";
 
-const BaseComment = ({ data }: any) => {
-  // const { userId, postId, media, content, createdAt, updatedAt } = data;
-  // we have to use the comment userId and hit get profile endpoint to get profile pic
-  // get single user from db
-  const userId = 1;
-  const postId = 5;
-  const media = "";
-  const content = "strangalanga";
-  const createdAt = new Date();
-  const month = createdAt.getMonth() + 1;
-  const year = createdAt.getFullYear();
-  const day = createdAt.getDate();
-  const hour = createdAt.getHours();
-  const minutes = createdAt.getMinutes();
-  const seconds = createdAt.getSeconds();
-  const datetime = `${hour}:${minutes}:${seconds} ${month}/${day}/${year}`;
-  const updatedAt = new Date();
-  const firstName = "john";
-  const lastName = "son";
-  const likes = 20;
-  const { themeColor } = useTheme();
-import { Card, Typography } from "@mui/material";
-import { BaseCommentPropType } from "../models/Comments.model";
-import useTheme from "features/theme/useTheme";
-
 const BaseComment = () => {
   // const { userId, postId, media, content, createdAt, updatedAt } = data;
-  // we have to use the comment userId and hit get profile endpoint to get profile pic
-  // get single user from db
   const userId = 1;
   const postId = 5;
   const media = "";
@@ -51,8 +24,7 @@ const BaseComment = () => {
   const isUserStylingProps = {
     display: "flex",
     justifyContent: "flex-start",
-    border: "2px solid green",
-    p: "10px 10px 10px",
+    p: "10px 10px 0px 30px",
   };
 
   const isNotUserStylingProps = {
@@ -66,20 +38,12 @@ const BaseComment = () => {
     <>
       {isUser ? (
         <Card variant="outlined" sx={isUserStylingProps}>
-          <Typography fontSize={8}>{content}</Typography>
-        </Card>
-      ) : (
-        <Card variant="outlined" sx={isNotUserStylingProps}>
-          <Typography fontSize={8}>
-            The Theory the the thumb was a thigh
-          </Typography>
-          <Box
+          <CardContent
             sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "flex-start",
-              maxWidth: "30%",
-              mt: "10px",
+              p: 0,
+              "&:last-child": {
+                pb: "5px",
+              },
             }}
           >
             <Box
@@ -90,6 +54,7 @@ const BaseComment = () => {
             >
               <IconButton
                 disableRipple
+              <Box
                 sx={{
                   display: "flex",
                   justifyContent: "center",
@@ -126,19 +91,62 @@ const BaseComment = () => {
               }}
             >
               <Button
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontSize: "10px",
+                    display: "flex",
+                    mb: "10px",
+                  }}
+                >
+                  {firstName} {lastName}
+                </Typography>
+                <Typography
+                  sx={{
+                    fontSize: "10px",
+                    display: "flex",
+                  }}
+                >
+                  {createdAt}
+                </Typography>
+              </Box>
+              <Typography fontSize={10}>
+                {content}The Theory the the thumb was a thighThe Theory the the
+                thumb was a thighThe Theory the the thumb was a thighThe Theory
+                the the thumb was a thighThe Theory the the thumb was a thighThe
+                Theory the the thumb was a thighThe Theory the the thumb was a
+                thighThe Theory the the thumb was a thighThe Theory the the
+                thumb was a thighThe Theory the the thumb was a thighThe Theory
+                the the thumb was a thighThe Theory the the thumb was a thighThe
+                Theory the the thumb was a thighThe Theory the the thumb was a
+                thighThe Theory the the thumb was a thighThe Theory the the
+                thumb was a thighThe Theory the the thumb was a thigh
+              </Typography>
+              <Box
                 sx={{
                   py: "5px",
                   color: themeColor.color,
                   mx: "10px",
                   "&:hover": { bgcolor: "transparent" },
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "flex-start",
+                  maxWidth: "30%",
+                  mt: "10px",
                 }}
               >
                 <AddComment
+                <Box
                   sx={{
                     fontSize: "13px",
                     mt: "2px",
                     mr: "5px",
                     color: themeColor.color,
+                    display: "flex",
+                    flexDirection: "row",
                   }}
                 />
                 <Typography sx={{ fontSize: "10px", textTransform: "none" }}>
@@ -150,6 +158,82 @@ const BaseComment = () => {
         </CardContent>
       </Box>
     </Card>
+                >
+                  <IconButton
+                    disableRipple
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      p: "0 3px 0 0",
+                      "&:hover": {
+                        bgcolor: themeColor.backgroundColor,
+                      },
+                    }}
+                  >
+                    <Favorite
+                      sx={{
+                        fontSize: "14px",
+                        color: themeColor.color,
+                        "&:hover": { color: "#E0115F" },
+                      }}
+                    />
+                  </IconButton>
+                  <Typography
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      fontSize: "10px",
+                      mt: "5px",
+                    }}
+                  >
+                    {likes}
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
+                  <Button
+                    sx={{
+                      py: "5px",
+                      color: themeColor.color,
+                      mx: "10px",
+                      "&:hover": { bgcolor: "transparent" },
+                    }}
+                  >
+                    <AddComment
+                      sx={{
+                        fontSize: "13px",
+                        mt: "2px",
+                        mr: "5px",
+                        color: themeColor.color,
+                      }}
+                    />
+                    <Typography
+                      sx={{ fontSize: "10px", textTransform: "none" }}
+                    >
+                      Reply
+                    </Typography>
+                  </Button>
+                </Box>
+              </Box>
+            </CardContent>
+          </Box>
+        </Card>
+      ) : (
+        <Card variant="outlined" sx={isNotUserStylingProps}>
+          <Typography>
+            {firstName} {lastName}
+          </Typography>
+          <Typography fontSize={8}>
+            The Theory the the thumb was a thigh
+          </Typography>
+        </Card>
+      )}
+    </>
   );
 };
 
