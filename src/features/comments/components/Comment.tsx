70bb1017394c0531c5ad0f8dfc295f5ee0b8f046
Daywick <derekli11204@gmail.com>
2023-01-15 23:04:22 -0500

    stash

--- src/features/comments/components/Comment.tsx
+++ src/features/comments/components/Comment.tsx
@@ -32,6 +32,33 @@ const BaseComment = ({ data }: any) => {
   const lastName = "son";
   const likes = 20;
   const { themeColor } = useTheme();
+import { Card, Typography } from "@mui/material";
+import { BaseCommentPropType } from "../models/Comments.model";
+import useTheme from "features/theme/useTheme";
+
+const BaseComment = () => {
+  // const { userId, postId, media, content, createdAt, updatedAt } = data;
+  const userId = 1;
+  const postId = 5;
+  const media = "";
+  const content = "strangalanga";
+  const createdAt = new Date();
+  const updatedAt = new Date();
+  const isUser: boolean = true;
+  const { themeColor } = useTheme();
+  const isUserStylingProps = {
+    display: "flex",
+    justifyContent: "flex-start",
+    border: "2px solid green",
+    p: "10px 10px 10px",
+  };
+
+  const isNotUserStylingProps = {
+    display: "flex",
+    justifyContent: "flex-start",
+    border: "1px solid black",
+    py: "10px",
+  };
 
   return (
     <Card
