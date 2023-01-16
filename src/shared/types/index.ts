fb231bccac73579c8c2f327c2b39666768efd661
Emir Akhmetov <91210955+emir4ik-code@users.noreply.github.com>
2023-01-16 15:39:20 -0500

    chat message done

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,4 +1,4 @@
 export { languageArray } from "./Language";
 export type { BasePostModel, BaseCreatePostmodel } from "./SharePostTypes";
-export type { UserProfileType } from "./SharedUserTypes";
+// export type { UserProfileType } from "./SharedUserTypes";
 export type { BasePostType, BasePostTopType, BasePostBodyType, BasePostFooterType } from "./Post";
