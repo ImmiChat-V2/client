f6ead04ddb644c8870a5513fa4e525bc0a87b5e2
ntan03 <91350990+ntan03@users.noreply.github.com>
2023-01-16 17:05:47 -0500

    Created chat top bar component

---
+++ src/features/chat/components/ChatTopBar.tsx
@@ -0,0 +1,51 @@
+import { UserProfileWidget } from "shared/components";
+import useTheme from "features/theme/useTheme";
+import { Box, IconButton } from "@mui/material";
+import { Phone, Videocam, Info } from "@mui/icons-material";
+
+type ChatTopBarProps = {
+  profilePicture?: string;
+  firstName: string;
+  lastName: string;
+};
+
+function ChatTopBar({ profilePicture, firstName, lastName }: ChatTopBarProps) {
+  const {
+    themeColor: { backgroundColor },
+  } = useTheme();
+
+  return (
+    <Box
+      component="div"
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "100%",
+        backgroundColor,
+      }}
+    >
+      <Box component="div" sx={{ p: 1 }}>
+        <UserProfileWidget
+          profilePicture={profilePicture}
+          firstName={firstName}
+          lastName={lastName}
+        />
+      </Box>
+
+      <Box component="div" sx={{ p: 1 }}>
+        <IconButton>
+          <Phone style={{ color: "#1877f2" }} />
+        </IconButton>
+        <IconButton>
+          <Videocam style={{ color: "#1877f2" }} />
+        </IconButton>
+        <IconButton>
+          <Info style={{ color: "#1877f2" }} />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+}
+
+export default ChatTopBar;
