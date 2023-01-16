14c5ae6b76cbe322202a18fba347610ee0c3ff85
Emir Akhmetov <91210955+emir4ik-code@users.noreply.github.com>
2023-01-16 15:55:12 -0500

    prettier

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,3 +1,8 @@
 export { languageArray } from "./Language";
 export type { BasePostModel, BaseCreatePostmodel } from "./SharePostTypes";
-export type { BasePostType, BasePostTopType, BasePostBodyType, BasePostFooterType } from "./Post";
+export type {
+  BasePostType,
+  BasePostTopType,
+  BasePostBodyType,
+  BasePostFooterType,
+} from "./Post";
