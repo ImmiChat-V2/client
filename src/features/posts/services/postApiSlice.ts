62c4702876087402df0471676b708fb7df29b876
Daywick <derekli11204@gmail.com>
2023-01-17 16:33:33 -0500

    working with redux endpoint

--- src/features/posts/services/postApiSlice.ts
+++ src/features/posts/services/postApiSlice.ts
@@ -1,62 +1,8 @@
 import { apiSlice } from "features/api/apiSlice";
-import {
-  BasePostModel,
-  BasePostRequestModel,
-  UpdatePostRequestModel,
-} from "../models/Posts.model";
+import { BasePostModel } from "../models/Posts.model";
 
 export const postApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    getPosts: builder.query<BasePostModel, null>({
-      query: () => ({
-        url: "/posts",
-        method: "GET",
-      }),
-    }),
-    getSinglePost: builder.query<BasePostModel, number>({
-      query: (postId) => ({
-        url: `/posts/${postId}`,
-        method: "GET",
-      }),
-    }),
-    createPost: builder.mutation<BasePostModel, BasePostRequestModel>({
-      query: (body) => ({
-        url: "/posts",
-        method: "POST",
-        body,
-      }),
-    }),
-    updatePost: builder.mutation<BasePostModel, UpdatePostRequestModel>({
-      query: ({ postId, body }) => ({
-        url: `/posts/${postId}`,
-        method: "PUT",
-        body,
-      }),
-    }),
-    deletePost: builder.mutation<null, number>({
-      query: (postId) => ({
-        url: `/posts/${postId}`,
-        method: "DELETE",
-      }),
-    }),
-    getLikesFromPost: builder.query<any, number>({
-      query: (postId) => ({
-        url: `/posts/${postId}/likes`,
-        method: "GET",
-      }),
-    }),
-    likePost: builder.mutation<string, number>({
-      query: (postId) => ({
-        url: `/posts/${postId}/likes`,
-        method: "POST",
-      }),
-    }),
-    deletePostLike: builder.mutation<string, number>({
-      query: (postId) => ({
-        url: `/posts/${postId}/likes`,
-        method: "DELETE",
-      }),
-    }),
     getPostsByUser: builder.query<{ data: BasePostModel[] }, number>({
       query: (userId) => ({
         url: `/users/${userId}/posts`,
@@ -66,14 +12,4 @@ export const postApiSlice = apiSlice.injectEndpoints({
   }),
 });
 
-export const {
-  useGetPostsQuery,
-  useGetSinglePostQuery,
-  useCreatePostMutation,
-  useUpdatePostMutation,
-  useDeletePostMutation,
-  useGetLikesFromPostQuery,
-  useLikePostMutation,
-  useDeletePostLikeMutation,
-  useGetPostsByUserQuery,
-} = postApiSlice;
+export const { useGetPostsByUserQuery } = postApiSlice;
