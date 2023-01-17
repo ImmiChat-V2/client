cd933d37ede78373382b35c59dc51a18630dba35
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2023-01-16 18:52:46 -0800

    display create post on homepage

--- src/screens/Home/HomePageBody.tsx
+++ src/screens/Home/HomePageBody.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import { Feed } from "features/feed/components";
 import useTheme from "features/theme/useTheme";
-import "./homePageBody.css"
+import "./homePageBody.css";
 
 function HomePageBody() {
   const {
@@ -9,55 +9,55 @@ function HomePageBody() {
     themeColor: { color, navButtons },
   } = useTheme();
   return (
-    <Box sx={{bgcolor: isDarkMode ? 'black' : 'white'}}>
-    <Box
-      className="home-container"
-      sx={{
-        display: "flex",
-        justifyContent: "space-around",
-        pt: "30px",
-        pr: '30px',
-        pl: '30px',
-        maxWidth: '1650px',
-        m: 'auto',
-      }}
-    >
-      <Box className="left-sidebar" sx={{ maxWidth: "400px" }}>
-        <Box
-          sx={{
-            bgcolor: navButtons,
-            height: "350px",
-            borderRadius: "10px",
-            p: "20px",
-            color,
-          }}
-        >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-          perspiciatis nulla facere ipsam praesentium illo incidunt fugiat. Sit
-          sunt rerum laudantium laboriosam perspiciatis consequuntur? Molestiae
-          quam quas provident inventore est!
+    <Box sx={{ bgcolor: isDarkMode ? "black" : "white" }}>
+      <Box
+        className="home-container"
+        sx={{
+          display: "flex",
+          justifyContent: "space-around",
+          pt: "30px",
+          pr: "30px",
+          pl: "30px",
+          maxWidth: "1650px",
+          m: "auto",
+        }}
+      >
+        <Box className="left-sidebar" sx={{ maxWidth: "400px" }}>
+          <Box
+            sx={{
+              bgcolor: navButtons,
+              height: "350px",
+              borderRadius: "10px",
+              p: "20px",
+              color,
+            }}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+            perspiciatis nulla facere ipsam praesentium illo incidunt fugiat.
+            Sit sunt rerum laudantium laboriosam perspiciatis consequuntur?
+            Molestiae quam quas provident inventore est!
+          </Box>
+        </Box>
+        <Box className="middle-feed" sx={{ ml: "15px", mr: "15px" }}>
+          <Feed />
+        </Box>
+        <Box className="right-sidebar" sx={{ maxWidth: "400px" }}>
+          <Box
+            sx={{
+              bgcolor: navButtons,
+              height: "500px",
+              borderRadius: "10px",
+              p: "20px",
+              color,
+            }}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+            perspiciatis nulla facere ipsam praesentium illo incidunt fugiat.
+            Sit sunt rerum laudantium laboriosam perspiciatis consequuntur?
+            Molestiae quam quas provident inventore est!
+          </Box>
         </Box>
       </Box>
-      <Box className="middle-feed" sx={{ ml: '15px', mr: '15px' }}>
-       <Feed />
-      </Box>
-      <Box className="right-sidebar" sx={{ maxWidth: "400px" }}>
-        <Box
-          sx={{
-            bgcolor: navButtons,
-            height: "500px",
-            borderRadius: "10px",
-            p: "20px",
-            color,
-          }}
-        >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-          perspiciatis nulla facere ipsam praesentium illo incidunt fugiat. Sit
-          sunt rerum laudantium laboriosam perspiciatis consequuntur? Molestiae
-          quam quas provident inventore est!
-        </Box>
-      </Box>
-    </Box>
     </Box>
   );
 }
